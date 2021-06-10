fee0daadd6e7146ca196186cffbfe22b41b4789c
likui911 <likui911@hotmail.com>
2021-06-10 20:44:35 +0800

    refs for Reactivity

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -2,19 +2,25 @@
   <div class="home">
     <p>My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
+    <button @click="age++">age++</button>
+    <input type="text" v-model="name" />
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "Home",
   setup() {
     console.log("setup");
-    let name = "mario";
-    let age = 30;
 
-    const handleClick = function () {
+    const name = ref("mario");
+    const age = ref(30);
+
+    const handleClick = () => {
       console.log("you clicked me");
+      name.value = "luigi";
+      age.value = 35;
     };
     return { name, age, handleClick };
   },
