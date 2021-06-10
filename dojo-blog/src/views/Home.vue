1d7d7fc88a69321ad84c97e7ce69989a0e4673a9
likui911 <likui911@hotmail.com>
2021-06-10 21:33:16 +0800

    Refs vs Reactive

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,28 +1,46 @@
 <template>
   <div class="home">
-    <p>My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">click me</button>
-    <button @click="age++">age++</button>
-    <input type="text" v-model="name" />
+    <h1>Refs</h1>
+    <h2>ninjaOne</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }} - {{ refName }}</p>
+    <button @click="handleNinjaOne">click me</button>
+
+    <h1>Reactive</h1>
+    <h2>ninjaTwo</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="handleNinjaTwo">click me</button>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
+
 export default {
   name: "Home",
   setup() {
-    console.log("setup");
-
-    const name = ref("mario");
-    const age = ref(30);
-
-    const handleClick = () => {
-      console.log("you clicked me");
-      name.value = "luigi";
-      age.value = 35;
+    //ref
+    const ninjaOne = ref({ name: "mario", age: 30 });
+    const refName = ref("mario");
+    const handleNinjaOne = () => {
+      ninjaOne.value.age++;
+      refName.value = "mario_1";
     };
-    return { name, age, handleClick };
+
+    //reactive
+    const ninjaTwo = reactive({ name: "luigi", age: 35 });
+    //reactivity.esm-bundler.js?a1e9:695 value cannot be made reactive
+    //const reaciveName = reactive("luigi");
+    const handleNinjaTwo = () => {
+      ninjaTwo.age++;
+    };
+
+    return {
+      ninjaOne,
+      refName,
+      handleNinjaOne,
+      ninjaTwo,
+      handleNinjaTwo,
+    };
   },
 };
 </script>
