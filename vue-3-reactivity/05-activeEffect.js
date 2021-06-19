ad017eec769a1b703b13ad20d39c9443a3be85e7
likui911 <likui911@hotmail.com>
2021-06-19 20:26:33 +0800

    Create activeEffect.js

---
+++ vue-3-reactivity/05-activeEffect.js
@@ -0,0 +1,78 @@
+const targetMap = new WeakMap();
+
+let activeEffect = null;
+
+function effect(eff) {
+    activeEffect = eff;
+    activeEffect();
+    activeEffect = null;
+}
+
+function track(target, key) {
+    if (activeEffect) {
+        let depsMap = targetMap.get(target)
+        if (!depsMap) {
+            targetMap.set(target, (depsMap = new Map()))
+        }
+        let dep = depsMap.get(key);
+        if (!dep) {
+            depsMap.set(key, (dep = new Set()))
+        }
+        dep.add(activeEffect)
+    }
+
+}
+
+function trigger(target, key) {
+    const depsMap = targetMap.get(target)
+    if (!depsMap) { return }
+    const dep = depsMap.get(key)
+    if (dep) {
+        dep.forEach(effect => {
+            effect()
+        });
+    }
+}
+
+function reactive(target) {
+    let handler = {
+        get(target, key, receiver) {
+            let result = Reflect.get(target, key, receiver)
+            track(target, key);
+            return result;
+        },
+        set(target, key, value, receiver) {
+            let oldValue = target[key];
+            let result = Reflect.set(target, key, value, receiver);
+            if (oldValue != result) {
+                trigger(target, key)
+            }
+            return result;
+        }
+    }
+    return new Proxy(target, handler);
+}
+
+let product = reactive({ price: 5, quantity: 2 })
+
+let salePrice = 0;
+let total = 0;
+
+effect(() => {
+    salePrice = product.price * 0.9
+})
+
+effect(() => {
+    total = product.price * product.quantity
+})
+
+
+
+
+console.log(`total = ${total}, salePrice = ${salePrice}`)
+
+product.quantity = 3;
+console.log(`total = ${total}, salePrice = ${salePrice}`)//15, 4.5
+
+product.price = 10
+console.log(`total = ${total}, salePrice = ${salePrice}`)//30, 9
