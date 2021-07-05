3d03d08ebef06a728a50a9e20edad54df063c825
likui911 <likui911@hotmail.com>
2021-07-05 22:22:18 +0800

    add global component: AppLink

--- conduit/src/components/PopularTags.vue
+++ conduit/src/components/PopularTags.vue
@@ -2,18 +2,30 @@
   <p>Popular Tags</p>
 
   <div class="tag-list">
-    <a href="" class="tag-pill tag-default" v-for="(tag, index) in tags">
+    <!-- :params="{ tag }" -->
+    <!-- Type 'UnwrappedObject<String>' is not assignable to type 'string'.ts(2322) -->
+    <app-link
+      v-for="(tag, index) in tags"
+      :key="index"
+      name="tag"
+      :params="{ tag: tag.toString() }"
+      class="tag-pill tag-default"
+    >
       {{ tag }}
-    </a>
+    </app-link>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "@vue/runtime-core";
 import { useTags } from "../composable/useTags";
+import AppLink from "../components/AppLink.vue";
 
 export default defineComponent({
   name: "PopularTags",
+  components: {
+    AppLink,
+  },
   async setup() {
     const { tags, fetchTags } = useTags();
     await fetchTags();
