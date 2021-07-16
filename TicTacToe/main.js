b41c3d18d310726840200ccbb131c01353f77e4b
likui911 <likui911@hotmail.com>
2021-07-16 11:07:25 +0800

    feat: click square

--- TicTacToe/main.js
+++ TicTacToe/main.js
@@ -10,7 +10,8 @@ app.component('Game', {
         <Board/>
     </div>
     <div className="game-info">
-        <div>Winner: X</div>
+    <!-- todo
+        <div>Winner: {{value}}</div>
         <ol>
             <li><button>Go to game start</button></li>
             <li><button>Go to move #1</button></li>
@@ -21,32 +22,56 @@ app.component('Game', {
             <li><button>Go to move #6</button></li>
             <li><button>Go to move #7</button></li>
         </ol>
+    -->
     </div>
   `,
+    setup() {
+    }
 })
 app.component('Board', {
     template: `
     <div>
+        <div className="status">{{ status }}</div>
         <div class="board-row">
-            <Square>X</Square>
-            <Square>X</Square>
-            <Square>X</Square>
+            <Square :value="squares[0]" @click="onClick(0)"/>
+            <Square :value="squares[1]" @click="onClick(1)"/>
+            <Square :value="squares[2]" @click="onClick(2)"/>
         </div>
         <div class="board-row">
-            <Square>X</Square>
-            <Square>O</Square>
-            <Square></Square>
+            <Square :value="squares[3]" @click="onClick(3)"/>
+            <Square :value="squares[4]" @click="onClick(4)"/>
+            <Square :value="squares[5]" @click="onClick(5)"/>
         </div>
         <div class="board-row">
-            <Square></Square>
-            <Square>O</Square>
-            <Square>O</Square>
+            <Square :value="squares[6]" @click="onClick(6)"/>
+            <Square :value="squares[7]" @click="onClick(7)"/>
+            <Square :value="squares[8]" @click="onClick(8)"/>
         </div>
     </div>
     `,
+    setup() {
+        const squares = reactive(Array(9).fill(''))
+        const xIsNext = ref(true);
+
+        const status = computed(() => {
+            return 'Next player: ' + (xIsNext.value ? 'X' : 'O');
+        });
+
+        const onClick = (i) => {
+            squares[i] = xIsNext.value ? 'X' : 'O';
+            xIsNext.value = !xIsNext.value;
+        }
+        return { squares, status, onClick }
+    }
 })
 
 app.component('Square', {
-    template: `<button class="square"><slot></slot></button>`
+    template: `<button class="square">{{value}}</button>`,
+    props: {
+        value: {
+            type: String,
+            required: true
+        }
+    }
 })
 app.mount(".game");
