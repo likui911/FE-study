c95a252f8c0cc6fb813e722620b0575af177cffc
likui911 <likui911@hotmail.com>
2021-07-17 16:08:17 +0800

    feat: display history

--- TicTacToe/main.js
+++ TicTacToe/main.js
@@ -7,53 +7,28 @@ const app = createApp({
 app.component('Game', {
     template: ` 
     <div className="game-board">
-        <Board/>
+        <Board @click="onClick" :squares="current" />
     </div>
     <div className="game-info">
-    <!-- todo
-        <div>Winner: {{value}}</div>
+        <div className="status">{{ status }}</div>
         <ol>
-            <li><button>Go to game start</button></li>
-            <li><button>Go to move #1</button></li>
-            <li><button>Go to move #2</button></li>
-            <li><button>Go to move #3</button></li>
-            <li><button>Go to move #4</button></li>
-            <li><button>Go to move #5</button></li>
-            <li><button>Go to move #6</button></li>
-            <li><button>Go to move #7</button></li>
+        <li v-for="move in moves" :key="move.step">
+            <button @click="jumpTo(move.step)">{{move.desc}}</button>
+        </li>
         </ol>
-    -->
     </div>
   `,
     setup() {
-    }
-})
-app.component('Board', {
-    template: `
-    <div>
-        <div className="status">{{ status }}</div>
-        <div class="board-row">
-            <Square :value="squares[0]" @click="onClick(0)"/>
-            <Square :value="squares[1]" @click="onClick(1)"/>
-            <Square :value="squares[2]" @click="onClick(2)"/>
-        </div>
-        <div class="board-row">
-            <Square :value="squares[3]" @click="onClick(3)"/>
-            <Square :value="squares[4]" @click="onClick(4)"/>
-            <Square :value="squares[5]" @click="onClick(5)"/>
-        </div>
-        <div class="board-row">
-            <Square :value="squares[6]" @click="onClick(6)"/>
-            <Square :value="squares[7]" @click="onClick(7)"/>
-            <Square :value="squares[8]" @click="onClick(8)"/>
-        </div>
-    </div>
-    `,
-    setup() {
-        const squares = reactive(Array(9).fill(''))
         const xIsNext = ref(true);
+        const stepNumber = ref(0);
+        const history = ref([Array(9).fill("")])
+
+        const current = computed(() => {
+            return history.value[history.value.length - 1]
+        })
 
         const status = computed(() => {
+            let squares = current.value.slice();
             const winner = calculateWinner(squares);
             let status;
             if (winner) {
@@ -65,13 +40,64 @@ app.component('Board', {
         });
 
         const onClick = (i) => {
+            let squares = current.value.slice();
             if (calculateWinner(squares) || squares[i]) {
                 return;
             }
             squares[i] = xIsNext.value ? 'X' : 'O';
             xIsNext.value = !xIsNext.value;
+            history.value.push(squares);
         }
-        return { squares, status, onClick }
+
+        const moves = computed(() => {
+            return history.value.map((square, step) => {
+                const desc = step ?
+                    'Go to move #' + step :
+                    'Go to game start';
+                return { step, desc }
+            })
+        })
+        const jumpTo = (step) => {
+            // todo
+            console.log(step)
+        }
+
+        return { current, moves, jumpTo, status, onClick }
+    }
+})
+app.component('Board', {
+    template: `
+    <div>
+        <div class="board-row">
+            <Square :value="squares[0]" @click.stop="onClick(0)"/>
+            <Square :value="squares[1]" @click.stop="onClick(1)"/>
+            <Square :value="squares[2]" @click.stop="onClick(2)"/>
+        </div>
+        <div class="board-row">
+            <Square :value="squares[3]" @click.stop="onClick(3)"/>
+            <Square :value="squares[4]" @click.stop="onClick(4)"/>
+            <Square :value="squares[5]" @click.stop="onClick(5)"/>
+        </div>
+        <div class="board-row">
+            <Square :value="squares[6]" @click.stop="onClick(6)"/>
+            <Square :value="squares[7]" @click.stop="onClick(7)"/>
+            <Square :value="squares[8]" @click.stop="onClick(8)"/>
+        </div>
+    </div>
+    `,
+    props: {
+        squares: {
+            type: Array,
+            required: true,
+            default: Array(9).fill(null)
+        }
+    },
+    setup(props, { emit }) {
+        const onClick = (i) => {
+            emit("click", i)
+        }
+
+        return { onClick }
     }
 })
 
