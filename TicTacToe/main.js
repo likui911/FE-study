8d857c2ea16779cc9d51033ddcf217fd916ae050
likui911 <likui911@hotmail.com>
2021-07-17 10:03:09 +0800

    feat: caculate winner

--- TicTacToe/main.js
+++ TicTacToe/main.js
@@ -54,10 +54,20 @@ app.component('Board', {
         const xIsNext = ref(true);
 
         const status = computed(() => {
-            return 'Next player: ' + (xIsNext.value ? 'X' : 'O');
+            const winner = calculateWinner(squares);
+            let status;
+            if (winner) {
+                status = 'Winner: ' + winner;
+            } else {
+                status = 'Next player: ' + (xIsNext.value ? 'X' : 'O');
+            }
+            return status;
         });
 
         const onClick = (i) => {
+            if (calculateWinner(squares) || squares[i]) {
+                return;
+            }
             squares[i] = xIsNext.value ? 'X' : 'O';
             xIsNext.value = !xIsNext.value;
         }
@@ -75,3 +85,23 @@ app.component('Square', {
     }
 })
 app.mount(".game");
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
